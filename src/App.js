9bfd36ec360d0d6e5f7e3e516040fbd81ce4dece
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2022-02-18 15:25:23 +0300

    init

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Sidebar from './components/Sidebar/Sidebar';
 import Tasks from './components/Tasks/Tasks';
 import History from "./components/History/History";
 
+console.log("init")
 function App() {
   console.log("START APP")
   return (
